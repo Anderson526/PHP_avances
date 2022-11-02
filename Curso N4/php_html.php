e75b530231be9d3cf77f8df04541f8d1aceb5576
AndersonC <78771740+Anderson526@users.noreply.github.com>
2022-11-02 16:33:19 -0500

    update

---
+++ Curso N4/php_html.php
@@ -0,0 +1,19 @@
+
+<?php
+$input= "Ingrese el input: <input type='text' placeholder='conceptos php'>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?= $input ?>
+
+
+</body>
+</html>
